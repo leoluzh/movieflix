367ba4e5833036e8cfe191365f3d4bb2b278b1ed
Leonardo Fernandes <leonardo.l.fernandes@gmail.com>
2021-03-24 20:50:38 -0300

    Criação de caso de teste para filme já registrado

--- src/test/java/com/lambdasys/movieflix/service/MovieServiceTest.java
+++ src/test/java/com/lambdasys/movieflix/service/MovieServiceTest.java
@@ -6,9 +6,13 @@ import com.lambdasys.movieflix.entity.Movie;
 import com.lambdasys.movieflix.exceptions.MovieAlreadyRegisteredException;
 import com.lambdasys.movieflix.mapper.MovieMapper;
 import com.lambdasys.movieflix.repository.MovieRepository;
+
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -16,6 +20,22 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.lessThan;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class MovieServiceTest {
@@ -49,17 +69,32 @@ public class MovieServiceTest {
         Movie expectedSaveMovie = movieMapper.toModel( expectedMovieDTO );
 
         // when - stubing mock
-        Mockito.when(movieRepository.findByName(expectedMovieDTO.getName())).thenReturn(Optional.empty());
-        Mockito.when(movieRepository.save(expectedSaveMovie)).thenReturn(expectedSaveMovie);
+        when(movieRepository.findByName(expectedMovieDTO.getName())).thenReturn(Optional.empty());
+        when(movieRepository.save(expectedSaveMovie)).thenReturn(expectedSaveMovie);
 
         // then
         MovieDTO createdMovieDTO = movieService.createMovie( expectedMovieDTO );
 
-        MatcherAssert.assertThat( createdMovieDTO.getId() , Matchers.is(  Matchers.equalTo( expectedMovieDTO.getId() ) ) );
-        MatcherAssert.assertThat( createdMovieDTO.getId() , Matchers.is(  Matchers.equalTo( expectedMovieDTO.getId() ) ) );
-        MatcherAssert.assertThat( createdMovieDTO.getId() , Matchers.is(  Matchers.equalTo( expectedMovieDTO.getId() ) ) );
+        assertThat( createdMovieDTO.getId() , Matchers.is(  Matchers.equalTo( expectedMovieDTO.getId() ) ) );
+        assertThat( createdMovieDTO.getId() , Matchers.is(  Matchers.equalTo( expectedMovieDTO.getId() ) ) );
+        assertThat( createdMovieDTO.getId() , Matchers.is(  Matchers.equalTo( expectedMovieDTO.getId() ) ) );
 
     }
 
+    @DisplayName("When an already registered movie is informed then an exception should be thrown")
+    @Test
+    public void whenAlreadyRegisteredMovieIsInformedThenAnExceptionShouldBeThrow(){
+
+        // given
+        MovieDTO expectedMovieDTO = MovieDTOBuilder.builder().build().toMovieDTO();
+        Movie duplicatedMovie = movieMapper.toModel( expectedMovieDTO );
+
+        // when
+        Mockito.when(movieRepository.findByName(expectedMovieDTO.getName())).thenReturn(Optional.of(duplicatedMovie));
+
+        // then
+        assertThrows( MovieAlreadyRegisteredException.class , () -> movieService.createMovie(expectedMovieDTO) );
+
+    }
 
 }
