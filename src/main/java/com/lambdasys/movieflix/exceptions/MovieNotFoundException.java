463be7b47c1c8559ae877b681ce5c744cc8e01f7
Leonardo Fernandes <leonardo.l.fernandes@gmail.com>
2021-03-27 01:57:18 -0300

    Inclusão de final no parametros e correção de estilo

--- src/main/java/com/lambdasys/movieflix/exceptions/MovieNotFoundException.java
+++ src/main/java/com/lambdasys/movieflix/exceptions/MovieNotFoundException.java
@@ -6,12 +6,12 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class MovieNotFoundException extends  Exception {
 
-    public MovieNotFoundException( String name ){
-        super(String.format("Movie with name %s not found in the system.",name));
+    public MovieNotFoundException(final String name) {
+        super(String.format("Movie with name %s not found in the system.", name));
     }
 
-    public MovieNotFoundException( Long id ){
-        super(String.format("Movie with id %s not found in the system.",id));
+    public MovieNotFoundException(final Long id) {
+        super(String.format("Movie with id %s not found in the system.", id));
     }
 
 }
